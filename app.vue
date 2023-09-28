7118dd2f214ff8a0764947177ab8b9655607853e
Nabezo1998 <rznabekura1203@gmail.com>
2023-09-28 17:15:32 +0900

    CRD 実装

--- app.vue
+++ app.vue
@@ -1,5 +1,47 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Todo App</h1>
+    <input v-model="taskName">
+    <button @click="taskName ? execUpdate(): addTask()">{{ buttonText }}</button>
+    <div
+      v-for="task in taskNameList"
+      :key="task"
+    >
+      {{ task }}
+      <button @click="completeTask(task)">削除</button>
+      <button @click="changeTask(task)">更新</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+  const taskName = ref<string>('');
+  const buttonText = computed(() => {
+    return 
+  })
+  const taskNameList =ref<string[]>(['html', 'css', 'Java Script']);
+  const addTask = () => {
+    if (taskName.value === '') {
+      return;
+    }
+    taskNameList.value.push(taskName.value);
+    taskName.value = '';
+  }
+  const completeTask = (completedTask:string) => {
+    taskNameList.value = taskNameList.value.filter((taskName:string) => {
+      return taskName !== completedTask;
+    })
+  }
+const changeTask = (task:string) => {
+  taskName.value = task;
+}
+const execUpdate = () => {
+  console.log('更新するよ');
+  
+  if (taskName.value === '') {
+      return;
+  }
+}
+
+
+</script>
